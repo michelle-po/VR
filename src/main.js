5a7ebab3e9fd0d4a952a65c0a3085ab4f82ef516
MichellePonti <michelle.ponti@heig-vd.ch>
2021-03-02 14:14:19 +0100

    chablolz base projet

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import "components/listen-to.js";
+import "components/hover-highlighter.js";
+import "components/grabbable.js";
+import "components/bind-rotation.js";
+import "components/disable-in-vr.js";
+import "components/chessboard.js";
+import "components/hexatile.js";
+import "components/navmesh-manager.js";
